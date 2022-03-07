5036d74a18b3121b1a35b72c0848221215cbe350
juani reil <juanireilluz@gmail.com>
2022-03-07 19:44:08 -0300

    clase y test prueba

---
+++ src/main/java/edu/fiuba/algo3/modelo/Inventario.java
@@ -0,0 +1,12 @@
+package edu.fiuba.algo3;
+
+import java.util.ArrayList;
+
+public class Inventario {
+
+    private ArrayList objetos;
+    public Inventario(){
+        objetos = new ArrayList<>();
+
+    }
+}
